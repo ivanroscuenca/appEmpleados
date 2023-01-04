e5873993a4cd61af8b6331fe96d90d83f76e2615
ivanroscuenca <ivanros@protonmail.com>
2023-01-04 10:22:46 +0100

    creado componente hijo

---
+++ src/app/empleado-hijo-c/empleado-hijo-c.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado-hijo-c',
+  templateUrl: './empleado-hijo-c.component.html',
+  styleUrls: ['./empleado-hijo-c.component.css']
+})
+export class EmpleadoHijoCComponent {
+
+}
