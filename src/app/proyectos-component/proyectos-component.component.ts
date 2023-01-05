dd4ac8756b9885e7dcccc2b5b89fdab34b48e0e2
ivanroscuenca <ivanros@protonmail.com>
2023-01-05 19:43:38 +0100

    video 26 routing botones

--- src/app/proyectos-component/proyectos-component.component.ts
+++ src/app/proyectos-component/proyectos-component.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Empleado } from '../empleado.model';
+import { EmpleadosService } from '../empleados.service';
+import { ServicioEmpleadosService } from '../servicio-empleados.service';
 
 @Component({
   selector: 'app-proyectos-component',
   templateUrl: './proyectos-component.component.html',
   styleUrls: ['./proyectos-component.component.css']
 })
-export class ProyectosComponentComponent {
+export class ProyectosComponentComponent implements OnInit {
+
+constructor(private router:Router, private miServicio: ServicioEmpleadosService, private empleadosService: EmpleadosService){
+
+}
+  ngOnInit(): void {
+    this.empleados = this.empleadosService.empleados;
+  }
+
+  volverHome(){
+
+    this.router.navigate([""]);
+
+  }
+
+  irContacto(){
+
+    this.router.navigate(["contacto"]);
+
+  }
+
+  empleados: Empleado[] = [];
+  cuadroNombre: string = "";
+  cuadroApellido: string = "";
+  cuadroCargo: string = "";
+  cuadroSalario: number = 0;
+
+  agregarEmpleado() {
+
+    //creamos una variable y guardamos datos
+    let miEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+
+    //usamos el servicio
+    //this.miServicio.muestraMensaje("Nombre del empleado: " + miEmpleado.nombre);
+
+    //llamamos al servicio
+    this.empleadosService.agregarEmpleadoServicio(miEmpleado);
+
+    this.router.navigate([""]);
+
+  }
 
 }
