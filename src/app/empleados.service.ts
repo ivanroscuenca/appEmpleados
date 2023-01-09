86ddd7831b284047db0883d289b1ba8faa25b29c
ivanroscuenca <ivanros@protonmail.com>
2023-01-09 10:11:19 +0100

    video 37 firebase delete

--- src/app/empleados.service.ts
+++ src/app/empleados.service.ts
@@ -6,55 +6,61 @@ import { Empleado } from "./empleado.model";
 import { ServicioEmpleadosService } from "./servicio-empleados.service";
 //decorador injectable nos dice que se va a inyectar
 @Injectable()
-export class EmpleadosService  {
+export class EmpleadosService {
   eliminarEmpleado(indice: number) {
-    this.empleados.splice(indice,1);
+    this.empleados.splice(indice, 1);
+    this.DataService.eliminarEmpleado(indice);
+    //utilizamos guardarempleados para volver a guardar el array
+    if (this.empleados != null) {
+      this.DataService.guardarEmpleados(this.empleados);
+    }
   }
+
   actualizarEmpleado(indice: number, empleado: Empleado) {
     let empleadoModificado = this.empleados[indice];
 
-    empleadoModificado.nombre=empleado.nombre;
-    empleadoModificado.apellido=empleado.apellido;
-    empleadoModificado.cargo=empleado.cargo;
-    empleadoModificado.salario=empleado.salario;
-    this.DataService.actualizarEmpleado(indice,empleado);
+    empleadoModificado.nombre = empleado.nombre;
+    empleadoModificado.apellido = empleado.apellido;
+    empleadoModificado.cargo = empleado.cargo;
+    empleadoModificado.salario = empleado.salario;
+    this.DataService.actualizarEmpleado(indice, empleado);
 
   }
   encontrarEmpleado(indice: number) {
-    let empleado:Empleado=this.empleados[indice];
+    let empleado: Empleado = this.empleados[indice];
     return empleado;
   }
 
   //necesitamos constructor para injectable
-  constructor(private servicioVentanaEmergente:ServicioEmpleadosService,private DataService:DataServices){
+  constructor(private servicioVentanaEmergente: ServicioEmpleadosService, private DataService: DataServices) {
 
   }
-  setEmpleados(misEmpleados:Empleado[]){
-    this.empleados=misEmpleados;
+  setEmpleados(misEmpleados: Empleado[]) {
+    this.empleados = misEmpleados;
   }
 
-  obtenerEmpleados(){
+  obtenerEmpleados() {
     return this.DataService.cargarEmpleados();
   }
-  empleados:Empleado[]=[];
+  empleados: Empleado[] = [];
 
-   /*empleados:Empleado[]= [
-        new Empleado("Juan","Díaz","Presidente",7500),
-        new Empleado("José","Fernandez","Gerente",5500),
-        new Empleado("María","Perez","Directora RRHH",3500),
-        new Empleado("Ana","Sánchez","Administrativa",1500)
-      ];*/
+  /*empleados:Empleado[]= [
+       new Empleado("Juan","Díaz","Presidente",7500),
+       new Empleado("José","Fernandez","Gerente",5500),
+       new Empleado("María","Perez","Directora RRHH",3500),
+       new Empleado("Ana","Sánchez","Administrativa",1500)
+     ];*/
 
-      agregarEmpleadoServicio(empleado:Empleado){
-         //llamar al servicio
-         this.servicioVentanaEmergente.muestraMensaje('Persona que se va a agregar' + "\n" + 
-        empleado.nombre + "\n"+ "salario: " + empleado.salario);
-         //Añadir dentro del array empleados
-        this.empleados.push(empleado);
-        //utilizamos guardar en data base
-        this.DataService.guardarEmpleados(this.empleados);
+  agregarEmpleadoServicio(empleado: Empleado) {
+    //llamar al servicio
+    this.servicioVentanaEmergente.muestraMensaje('Persona que se va a agregar' + "\n" +
+      empleado.nombre + "\n" + "salario: " + empleado.salario);
+    //Añadir dentro del array empleados
+    this.empleados.push(empleado);
+    //utilizamos guardar en data base
+    this.DataService.guardarEmpleados(this.empleados);
 
-      }
+  }
 
 
 
